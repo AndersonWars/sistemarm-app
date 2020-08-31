26194fb6c6e4f86aefc68151620ac9549c9291ed
Anderson Cardoso Gaspar <anderson.gaspar@unisul.br>
2020-08-31 13:45:41 -0300

    alteração heroku

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://sistemarm-teste.herokuapp.com/'
 })
 
 class ApiService {
