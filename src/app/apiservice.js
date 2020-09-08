8fc364d77ee03c25a2bed1a2b18a5eec2563a657
Anderson Cardoso Gaspar <anderson.gaspar@unisul.br>
2020-09-07 23:57:34 -0300

    local host

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-    baseURL: 'https://sistemarm-teste.herokuapp.com'
+    baseURL: 'http://localhost:8080'
 })
 
 class ApiService {
